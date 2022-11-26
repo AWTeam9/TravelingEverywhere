8830a34540a5f79716643fa0da76532a70f071a2
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-26 22:47:21 +0900

    feat(select.html): 각 테마별 이미지 개수에 맞게 출력

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,16 +1,26 @@
-const themeName = document.querySelector(".theme-name");
-const scrollMenu = document.querySelector(".scroll-menu");
+const THEME_SIZE = 4;
+const themeNames = ["바다", "산", "도시", "마을"];
+const folderNames = ["beach", "mountain", "city", "village"];
+const themeItemNumbers = [5, 4, 5, 6];
 
-const themeNames = ["바다", "산", "마을", "도시"];
+for(var i = 0; i < THEME_SIZE; i++){
+    const theme = document.createElement("div");
 
-const fileName = "1.jpg"
-const filePath = `../img/theme/${fileName}`;
-
-for(var i = 0; i < 4; i++) {
+    // 테마 이름 출력 
+    const themeName = document.createElement("h2");
     themeName.innerText = themeNames[i];
+    theme.appendChild(themeName);
+
+    // 각 테마별 이미지 개수에 맞게 출력 
+    const scrollMenu = document.createElement("div");
+    scrollMenu.setAttribute("class", "scroll-menu");
+
+    for(var j = 0; j < themeItemNumbers[i]; j++) {
+        const img = document.createElement("img");
+        img.src = `../img/theme/${folderNames[i]}/${j+1}.jpg`;
+        scrollMenu.appendChild(img);
+    }
+    theme.appendChild(scrollMenu);
+
+    document.body.appendChild(theme);
 }
-
-const themeImg = document.createElement("img");
-themeImg.src = filePath
-scrollMenu.appendChild(themeImg);
-
