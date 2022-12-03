750a8e12c16c3ee1920fa601b4d5a31462a02c22
leeeha <jxlhe46@seoultech.ac.kr>
2022-12-03 23:04:52 +0900

    feat(select): header, footer 구현

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,16 +1,19 @@
+const contents = document.querySelector(".contents"); 
+
 const THEME_SIZE = 4;
 const themeNames = ["바다", "산", "도시", "마을"];
 const folderNames = ["beach", "mountain", "city", "village"];
 const themeItemNumbers = [5, 4, 5, 6];
 
 for(var i = 0; i < THEME_SIZE; i++){
-    const theme = document.createElement("div");
+    const themeList = document.createElement("div");
+    themeList.setAttribute("class", "theme-list");
 
     // 테마 이름 출력 
     const themeName = document.createElement("h2");
     themeName.setAttribute("class", "theme-name");
     themeName.innerText = themeNames[i];
-    theme.appendChild(themeName);
+    themeList.appendChild(themeName);
 
     // 각 테마별 이미지 개수에 맞게 출력 
     const scrollMenu = document.createElement("div");
@@ -25,7 +28,7 @@ for(var i = 0; i < THEME_SIZE; i++){
         link.appendChild(img);
         scrollMenu.appendChild(link);
     }
-    theme.appendChild(scrollMenu);
-
-    document.body.appendChild(theme);
+    themeList.appendChild(scrollMenu);
+    
+    contents.appendChild(themeList);
 }
