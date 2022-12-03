82a1be9e5b1a8ed8404635de32c79ce3a2594df6
leeeha <jxlhe46@seoultech.ac.kr>
2022-12-03 17:54:24 +0900

    feat(main): 라디오 재생/중지 버튼 추가

--- public/js/sidepanel.js
+++ public/js/sidepanel.js
@@ -1,5 +1,2 @@
-const toggleButton = document.querySelector(".side-panel-toggle")
 
-toggleButton.addEventListener("click", () => {
-    document.querySelector(".wrapper").classList.toggle("side-panel-open");
-})
+
