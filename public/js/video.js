f0e8804bada07a6a83f21811edf3cb9c9e7cc633
leeeha <jxlhe46@seoultech.ac.kr>
2022-12-10 00:24:27 +0900

    feat: 영상 사운드 mute/unmute 기능 구현

--- public/js/video.js
+++ public/js/video.js
@@ -1,35 +1,57 @@
-const iframe = document.querySelector("iframe");
-const url = document.getElementById("url");
-const videoControl = "?autoplay=1&mute=1&loop=1&controls=0&disablekb=1&start=25"
+const videoSoundButton = document.querySelector("#video-sound-btn");
 
-const videoUrl = url.value;
-//const videoUrl = "https://www.youtube.com/embed/lOGE7nNkL0Y"
-iframe.src = videoUrl + videoControl;
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
 
-// var tag = document.createElement('script');
-// tag.src = "https://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//const url = document.getElementById("url");
+//const videoUrl = url.value;
+const _videoId = "lOGE7nNkL0Y";
+//const videoControl = "?autoplay=1&mute=1&loop=1&controls=0&disablekb=1&start=25";
 
-// var player;
-// function onYouTubeIframeAPIReady() {
-//     player = new YT.Player('player', {
-//         //videoId: 'LdIjNVHCVsc',
-//         playerVars: {
-//             autoplay: 1,
-//             mute: 1,
-//             loop: 1,
-//             controls: 0,
-//             disablekb: 1,
-//             start:25,
-//             //playlist: 'LdIjNVHCVsc'
-//         },
-//         events: {
-//             onReady: function (e) {
-//                 // e.target.mute();
-//                 e.target.setPlaybackRate(0.5); // set to half speed
-//             }
-//         }
-//     });
-// }
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-player', {
+        videoId: _videoId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+        playerVars: {
+            'autoplay': 1, 
+            'mute': 1,
+            'loop': 1, 
+            'controls': 0,
+            'disablekb': 1,
+            'start': 25
+        },
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+
+}
+
+// 5. The API calls this function when the player's state changes.
+function onPlayerStateChange(event) {
+
+}
+
+videoSoundButton.addEventListener("click", () => {
+    if(videoSoundButton.innerText == 'ON') {
+        console.log(videoSoundButton.innerText);
+        videoSoundButton.innerText = 'OFF';
+        player.unMute();
+    }else {
+        console.log(videoSoundButton.innerText);
+        videoSoundButton.innerText = 'ON';
+        player.mute();
+    }
+})
